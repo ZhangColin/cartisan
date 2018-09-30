0e0537256aea4d51065e8c2f135c6cccedfa9219
文野 <zhang_colin@163.com>
2018-09-30 09:48:41 +0800

    Management 添加首页

---
+++ cartisan-management/src/main/java/com/cartisan/management/modules/sys/controller/SystemController.java
@@ -0,0 +1,20 @@
+package com.cartisan.management.modules.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>Title: SystemController</p>
+ * <p>Description: </p>
+ *
+ * @author colin
+ */
+@Controller
+public class SystemController {
+    @RequestMapping(value = {"/", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+
+}
