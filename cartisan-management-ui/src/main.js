403d05d8aed7f33d413e1a16156aaec990d872ea
文野 <zhang_colin@163.com>
2018-10-09 17:41:36 +0800

    初始化后台入口页

--- cartisan-management-ui/src/main.js
+++ cartisan-management-ui/src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './lib/jquery-vender.js'
+import 'bootstrap'
+import 'admin-lte'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
 
 Vue.config.productionTip = false
 
