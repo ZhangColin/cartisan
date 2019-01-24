69bafd3de4d5dbfac46dee551a879ada757fde66
文野 <zhang_colin@163.com>
2019-01-25 01:30:41 +0800

    management: 更新management-ui在staging环境下的配置

--- cartisan-management-ui/config/staging.env.js
+++ cartisan-management-ui/config/staging.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"staging"',
-  BASE_API: '"https://api-staging"'
+  // BASE_API: '"https://api-staging"'
+  BASE_API: '"http://192.168.2.13/management/"'
 }
