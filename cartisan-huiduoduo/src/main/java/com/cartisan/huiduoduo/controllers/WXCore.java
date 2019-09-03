1cbd1ecd73d1167c0b74cb6615aed7519127a9c0
文野 <zhang_colin@163.com>
2019-09-03 22:52:07 +0800

    fix: remove WXCore main method

--- cartisan-huiduoduo/src/main/java/com/cartisan/huiduoduo/controllers/WXCore.java
+++ cartisan-huiduoduo/src/main/java/com/cartisan/huiduoduo/controllers/WXCore.java
@@ -35,14 +35,14 @@ public class WXCore {
     }
 
 
-    public static void main(String[] args) throws Exception{
-        String appId = "wxaa496b8f36ca4d99";
-        String encryptedData = "M/o32rsHG6adX6TaL5eyuB1TMLrD1PCPOJfvRwg9ycVoLQwzL5JUct3NzGqELAK4VAwOAy6wFUXw1XP8/bS/mDTW381c8Bb5i11ARbilLElBb3WlO6HeAwYhPVoFMWnJC2TxWOqq3PmUtTqbmrOu7X+XddGB93EGLBQ33n+KY4M5QQcna9yAHXuDQd01By+qqiGl+YNJNC7HuG/FcooC7Gb+mlBhwCWY2BvdSDmONp7Zp8XpW+QaKKzqPj7d7KeJSWhn1U0qRXd6K6I++J6MukeAcJ2yXVzJ0uccfzuTw5x+Wy1160QAXmb6ZYCtFbW+kl5kcVAPkvDoEzrirJSM8D06uhRsN1dG3r7i/cEuB9L0pTtQGmb5UZuPB5GlXg3HBzYpXM3VgOoXaIAANyEVvd3TIDB+keTZxQr93F6sVwoLs0j7UuE1AaCQOJaeWYSc3mxV4R3IKWlRkXIQS8XMMiaJjY5/R7MULyXwMDhWyKY1GfhTbRPryBQL89OJvbR2";
-        String sessionKey = "wwfp7urwPE6yHECfh7KKFw==";
-        String iv = "5SXRk9WzwoRc9Spep85x4Q==";
-        final String decrypt = decrypt(appId, encryptedData, sessionKey, iv);
-        System.out.println(decrypt.length());
-        System.out.println(decrypt);
-    }
+//    public static void main(String[] args) throws Exception{
+//        String appId = "wxaa496b8f36ca4d99";
+//        String encryptedData = "M/o32rsHG6adX6TaL5eyuB1TMLrD1PCPOJfvRwg9ycVoLQwzL5JUct3NzGqELAK4VAwOAy6wFUXw1XP8/bS/mDTW381c8Bb5i11ARbilLElBb3WlO6HeAwYhPVoFMWnJC2TxWOqq3PmUtTqbmrOu7X+XddGB93EGLBQ33n+KY4M5QQcna9yAHXuDQd01By+qqiGl+YNJNC7HuG/FcooC7Gb+mlBhwCWY2BvdSDmONp7Zp8XpW+QaKKzqPj7d7KeJSWhn1U0qRXd6K6I++J6MukeAcJ2yXVzJ0uccfzuTw5x+Wy1160QAXmb6ZYCtFbW+kl5kcVAPkvDoEzrirJSM8D06uhRsN1dG3r7i/cEuB9L0pTtQGmb5UZuPB5GlXg3HBzYpXM3VgOoXaIAANyEVvd3TIDB+keTZxQr93F6sVwoLs0j7UuE1AaCQOJaeWYSc3mxV4R3IKWlRkXIQS8XMMiaJjY5/R7MULyXwMDhWyKY1GfhTbRPryBQL89OJvbR2";
+//        String sessionKey = "wwfp7urwPE6yHECfh7KKFw==";
+//        String iv = "5SXRk9WzwoRc9Spep85x4Q==";
+//        final String decrypt = decrypt(appId, encryptedData, sessionKey, iv);
+//        System.out.println(decrypt.length());
+//        System.out.println(decrypt);
+//    }
 
 }
