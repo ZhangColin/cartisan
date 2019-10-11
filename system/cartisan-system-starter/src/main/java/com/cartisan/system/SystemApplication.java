07151325ffd64d419df59c44f71c6b824b2b9d8c
文野 <zhang_colin@163.com>
2019-10-11 14:35:22 +0800

    调整日志配置文件结构

--- system/cartisan-system-starter/src/main/java/com/cartisan/system/SystemApplication.java
+++ system/cartisan-system-starter/src/main/java/com/cartisan/system/SystemApplication.java
@@ -1,18 +1,14 @@
 package com.cartisan.system;
 
-import com.cartisan.common.repositories.CartisanRepositoryFactoryBean;
 import com.cartisan.common.utils.SnowflakeIdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author colin
  */
 @SpringBootApplication(scanBasePackages = {"com.cartisan.common", "com.cartisan.system"})
-@EnableJpaRepositories(basePackages = {"com.cartisan.system"},
-        repositoryFactoryBeanClass = CartisanRepositoryFactoryBean.class)
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class);
--- system/cartisan-system/src/main/java/com/cartisan/system/config/SystemConfig.java
+++ system/cartisan-system/src/main/java/com/cartisan/system/config/SystemConfig.java
@@ -1,16 +1,13 @@
 package com.cartisan.system.config;
 
-import com.cartisan.common.repositories.CartisanRepositoryFactoryBean;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author colin
  */
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.cartisan.system"},
-        repositoryFactoryBeanClass = CartisanRepositoryFactoryBean.class)
-@EntityScan(basePackages = {"com.cartisan.system"})
+//@EnableJpaRepositories(basePackages = {"com.cartisan.system"},
+//        repositoryFactoryBeanClass = CartisanRepositoryFactoryBean.class)
+//@EntityScan(basePackages = {"com.cartisan.system"})
 public class SystemConfig {
 }
