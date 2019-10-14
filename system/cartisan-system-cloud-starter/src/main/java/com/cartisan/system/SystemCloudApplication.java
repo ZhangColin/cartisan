56373d7bf40af4459e31ab9f80d57059a208cfda
文野 <zhang_colin@163.com>
2019-10-14 22:57:02 +0800

    更新SystemCloudApplication

--- system/cartisan-system-cloud-starter/src/main/java/com/cartisan/system/SystemCloudApplication.java
+++ system/cartisan-system-cloud-starter/src/main/java/com/cartisan/system/SystemCloudApplication.java
@@ -1,14 +1,18 @@
 package com.cartisan.system;
 
 import com.cartisan.common.utils.SnowflakeIdWorker;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author colin
  */
 @SpringCloudApplication
+@ComponentScan(basePackages = {"com.cartisan.common", "com.cartisan.system"})
+@MapperScan("com.cartisan.system.queries")
 public class SystemCloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemCloudApplication.class);
