9943e7bef624aad5e3cdd2c1a5972037b1bfe6b3
colin <zhang_colin@163.com>
2024-11-07 10:06:01 +0800

    项目初始化

---
+++ cartisan/src/main/java/com/cartisan/CartisanApplication.java
@@ -0,0 +1,14 @@
+package com.cartisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangcolin
+ */
+@SpringBootApplication
+public class CartisanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartisanApplication.class, args);
+    }
+}
